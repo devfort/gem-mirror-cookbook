35cabf586e832f5941bae7a0c81ca0ae5b62c29e
Steve Marshall <steve@nascentguruism.com>
2013-08-29 22:39:58 +0100

    Remove unnecessary attribute overrides.

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,6 +5,3 @@ default['gem_mirror']['apache']['listen_hostname'] = '*'
 default['gem_mirror']['apache']['listen_port'] = '80'
 
 override['apache']['default_site_enabled'] = false
-
-override[:rbenv][:git_repository] = "https://github.com/sstephenson/rbenv.git"
-override[:ruby_build][:git_repository] = "https://github.com/sstephenson/ruby-build.git"
