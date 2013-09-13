4ee2a269f9f395c3cf7a9260aa5c7d8f5d7ee6d5
Steve Marshall <steve@nascentguruism.com>
2013-09-13 21:20:25 +0100

    Set user, dumbo.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
-default['gem_mirror']['data_dir'] = '/data/rubygems'
 default['gem_mirror']['user'] = 'fort'
+default['gem_mirror']['data_dir'] = '/data/rubygems'
 
 default['gem_mirror']['apache']['listen_hostname'] = '*'
 default['gem_mirror']['apache']['listen_port'] = '80'
