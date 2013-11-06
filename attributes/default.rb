d9afd943f6d1038ce7a52cf7b06eb83cad2fe897
Steve Marshall <steve@nascentguruism.com>
2013-11-06 16:33:52 +0000

    Attributify remote host.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,6 @@
 default['gem_mirror']['user'] = 'fort'
 default['gem_mirror']['data_dir'] = '/data/rubygems'
+default['gem_mirror']['remote'] = 'https://rubygems.org'
 
 default['gem_mirror']['apache']['listen_hostname'] = 'gem.fort'
 default['gem_mirror']['apache']['listen_port'] = '80'
